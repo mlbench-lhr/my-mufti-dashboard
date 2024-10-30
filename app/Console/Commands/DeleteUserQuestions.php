cad41a100c7ecbc7100ccc5abf5f260b648bcac1
“Numan-star” <5125.2019.gct@gmail.com>
2024-10-30 16:51:47 +0500

    fixed issue

--- app/Console/Commands/DeleteUserQuestions.php
+++ app/Console/Commands/DeleteUserQuestions.php
@@ -1,47 +1,36 @@
 <?php
 
 namespace App\Console\Commands;
-use App\Models\ReportQuestion; 
+
 use App\Models\AdminReply;
-use App\Models\ScholarReply;
-use Illuminate\Console\Command;
-use App\Models\UserQuery;
-use App\Models\UserAllQuery;
 use App\Models\Question;
 use App\Models\QuestionComment;
 use App\Models\QuestionVote;
+use App\Models\ReportQuestion;
+use App\Models\ScholarReply;
+use App\Models\UserAllQuery;
+use App\Models\UserQuery;
+use Illuminate\Console\Command;
 
 class DeleteUserQuestions extends Command
 {
     protected $signature = 'questions:delete';
     protected $description = 'Delete all questions for specific users';
 
-    /**
-     * Execute the console command.
-     *
-     * @return void
-     */
     public function handle()
     {
         $specificUserIds = [380, 385, 253, 15, 206, 269, 24, 285, 9, 76, 348, 243];
 
         $questions = UserQuery::whereIn('user_id', $specificUserIds)->pluck('id')->toArray();
+        $public_questions = Question::whereIn('user_id', $specificUserIds)->pluck('id')->toArray();
 
-        if (empty($questions)) {
+        if (empty($public_questions) && empty($questions)) {
             $this->info('No questions found for the specified users.');
             return;
         }
 
         UserAllQuery::whereIn('query_id', $questions)->delete();
         UserQuery::whereIn('user_id', $specificUserIds)->delete();
-
-        $public_questions = Question::whereIn('user_id', $specificUserIds)->pluck('id')->toArray();
-
-        if (empty($public_questions)) {
-            $this->info('No questions found for the specified users.');
-            return;
-        }
-
 
         QuestionComment::whereIn('question_id', $public_questions)->delete();
         QuestionVote::whereIn('question_id', $public_questions)->delete();
@@ -50,8 +39,6 @@ class DeleteUserQuestions extends Command
         AdminReply::whereIn('question_id', $public_questions)->delete();
         Question::whereIn('user_id', $specificUserIds)->delete();
 
-
         $this->info('All questions for the specified users have been deleted.');
-
     }
 }
