64f1dc7d78a4b59a5b8aa14df67aedbdf99e1d29
“Numan-star” <5125.2019.gct@gmail.com>
2025-02-07 15:20:19 +0500

    add new apis

--- database/migrations/2025_02_07_063453_add_column_to_users_table.php
+++ database/migrations/2025_02_07_063453_add_column_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->longText('deletion_reason')->after('user_type')->default('');
+            $table->longText('deletion_reason')->after('user_type');
         });
     }
 
